b8e7feac2a8cecf3ebe6632b7e6ce04231e430e9
AntekC1 <41149199+AntonC1@users.noreply.github.com>
2021-09-09 07:34:13 +0200

    Add files via upload

---
+++ newslettter.php
@@ -0,0 +1,31 @@
+ <div id="newsletter" class="section">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<div class="newsletter">
+							<p>Zarejestruj się na <strong>AKTUALIZACJE OFERTY</strong></p>
+							<form id="offer_form" onsubmit="return false">
+								<input class="input" type="email" id="email" name="email" placeholder="Podaj swój Email">
+								<button class="newsletter-btn" value="Sign Up" name="signup_button" type="submit"><i class="fa fa-envelope"></i> Subskrybuj</button>
+							</form>
+							<div class="" id="offer_msg">
+                            </div>
+							<ul class="newsletter-follow">
+								<li>
+									<a href="https://www.facebook.com"><i class="fa fa-facebook"></i></a>
+								</li>
+								<li>
+									<a href="https://twitter.com/?lang=en"><i class="fa fa-twitter"></i></a>
+								</li>
+								<li>
+									<a href="https://www.instagram.com"><i class="fa fa-instagram"></i></a>
+								</li>
+								<li>
+									<a href="https://github.com/AntonC1"><i class="fa fa-github"></i></a>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
